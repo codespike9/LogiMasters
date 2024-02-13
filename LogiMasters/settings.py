8de497f1732fe59caff91e1f43488335d8515868
Dharmaraj <dharmarajjena694@gmail.com>
2024-02-13 23:59:43 +0530

    second commit

--- LogiMasters/settings.py
+++ LogiMasters/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -80,23 +80,29 @@ ASGI_APPLICATION = 'LogiMasters.asgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'logimasters',
-        'USER': 'postgres',
-        'PASSWORD': '21beeb10',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+# DATABASES={
+#     'default':dj_database_url.parse("postgres://logimasters_user:gyGXdciIp6Hd7c9e9kpMZhQP4dsWIDlU@dpg-cn5qko7109ks73a0coog-a/logimasters")
+# }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'logimasters',
+#         'USER': 'postgres',
+#         'PASSWORD': '21beeb10',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+DATABASES["default"]=dj_database_url.parse("postgres://logimasters_user:gyGXdciIp6Hd7c9e9kpMZhQP4dsWIDlU@dpg-cn5qko7109ks73a0coog-a.oregon-postgres.render.com/logimasters")
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
